5099e3f6206ea72215653c785512e6b10c222889
kevin lopes <kevinlp@live.com>
2020-10-13 20:28:49 -0300

    Subida do projeto inicial montado por breno e lauro e pequenas correções

---
+++ projeto/app/src/main/java/com/uniso/lpdm/foodsocial/foodsocial.java
@@ -0,0 +1,14 @@
+package com.uniso.lpdm.foodsocial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class foodsocial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
