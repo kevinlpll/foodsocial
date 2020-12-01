2971b090b91ed3fd9083a34752bf9ec00e32f106
kevin lopes <kevinlp@live.com>
2020-12-01 20:38:07 -0300

    Mudança de endereço da API

--- projeto/app/src/main/java/com/uniso/lpdm/foodsocial/services/RetrofitConfig.java
+++ projeto/app/src/main/java/com/uniso/lpdm/foodsocial/services/RetrofitConfig.java
@@ -12,7 +12,7 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 public class RetrofitConfig {
     private final Retrofit retrofit;
     public RetrofitConfig(){
-        this.retrofit =  new Retrofit.Builder().baseUrl("http://foodsocial-env.eba-7aqppnqr.us-east-2.elasticbeanstalk.com/")
+        this.retrofit =  new Retrofit.Builder().baseUrl("http://foodsocial.us-east-2.elasticbeanstalk.com/")
             .addConverterFactory(JacksonConverterFactory.create())
             .build();
     }
